0281320687155afa4227fc9ba6369d35bbda20cf
ralipnic <ali.alipoor94@gmail.com>
2026-06-12 15:36:09 +0330

    fix robots sitemap url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://ralipnic.com/sitemap.xml",
+    sitemap:
+      "https://ralipnic-site-git-main-ralipnics-projects.vercel.app/sitemap.xml",
   };
 }
