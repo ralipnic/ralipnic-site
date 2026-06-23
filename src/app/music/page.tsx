608cdbaf8a1e34fc0d1b4c0c4dfacd11d733a54b
ralipnic <ali.alipoor94@gmail.com>
2026-06-24 02:12:26 +0330

    Fix music page

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,8 +1,5 @@
+import MusicPageClient from "./MusicPageClient";
+
 export default function MusicPage() {
-  return (
-    <div>
-      <h1>Music</h1>
-      <p>ورود به پروژه</p>
-    </div>
-  );
+  return <MusicPageClient />;
 }
